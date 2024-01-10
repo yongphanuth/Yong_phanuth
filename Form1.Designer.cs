050951ee38000d3ecc8b5f05e4f2c86fe11f94a6
unknown <phanuth16112004@gmail.com>
2024-01-10 17:10:00 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace listview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.camboGender = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.add = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(386, 28);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(180, 26);
+            this.textName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gender ";
+            // 
+            // textPhone
+            // 
+            this.textPhone.Location = new System.Drawing.Point(386, 136);
+            this.textPhone.Name = "textPhone";
+            this.textPhone.Size = new System.Drawing.Size(180, 26);
+            this.textPhone.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone";
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(386, 191);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(180, 26);
+            this.textAddress.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Address";
+            // 
+            // camboGender
+            // 
+            this.camboGender.FormattingEnabled = true;
+            this.camboGender.Items.AddRange(new object[] {
+            "Male",
+            "Famale"});
+            this.camboGender.Location = new System.Drawing.Point(386, 84);
+            this.camboGender.Name = "camboGender";
+            this.camboGender.Size = new System.Drawing.Size(180, 28);
+            this.camboGender.TabIndex = 8;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(102, 247);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(700, 189);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Stu_Name";
+            this.columnHeader1.Width = 141;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Gender ";
+            this.columnHeader2.Width = 112;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Phone";
+            this.columnHeader3.Width = 146;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Address";
+            this.columnHeader4.Width = 240;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(635, 34);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 32);
+            this.add.TabIndex = 10;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(635, 84);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 32);
+            this.Delete.TabIndex = 11;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(635, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 448);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.camboGender);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textPhone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox camboGender;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.FullRowSelect = true;
+            /*String[] rr = new string[4];
+            ListViewItem itm;
+            rr[0] = "Damrith";
+            rr[1] = "Male";
+            rr[2] = "012345678";
+            rr[3] = "phnom penh #271";
+            itm = new ListViewItem(rr);
+            listView1.Items.Add(itm);*/
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            String[] storeData = new String [4];
+            ListViewItem itm;
+            storeData[0] = textName.Text;
+            storeData[1] = camboGender.Text;
+            storeData[2] = textPhone.Text;
+            storeData[3] = textAddress.Text;
+            itm = new ListViewItem(storeData);
+            listView1.Items.Add(itm);
+            textName.Clear();textPhone.Clear();textAddress.Clear();
+            camboGender.SelectedIndex = -1;
+            textName.Focus();
+
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listView1.Items.Count; i += 1)
+            {
+                if (listView1.Items[i].Selected)
+                {
+                    listView1.Items[i].Remove();
+                    i--;
+                }
+            }
+        }
+    }
+}
